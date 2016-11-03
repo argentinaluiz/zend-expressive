99fc647a55ebd0209da2e46c9e4a9b06aacf34b5
Luiz <argentinaluiz@gmail.com>
2016-11-03 14:58:23 -0200

    Reparando configuração do Doctrine ORM

--- config/autoload/doctrine.global.php
+++ config/autoload/doctrine.global.php
@@ -8,11 +8,11 @@ return [
                 'params' => [
                     'host'          => 'localhost',
                     'port'          => '3306',
-                    'user'          => 'homestead',
-                    'password'      => 'secret',
-                    'dbname'        => 'newPanel',
+                    'user'          => 'root',
+                    'password'      => 'root',
+                    'dbname'        => 'code_php7',
                     'driverOptions' => [
-                        \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF-8'"
+                        \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'UTF8'"
                     ]
                 ]
             ]
